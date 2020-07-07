8f17844b26e90ffbe6e847df3ec7e8d3e8074d41
vanof <58184758+vanof@users.noreply.github.com>
2020-07-07 16:54:16 +0300

    dht11 realisation

--- temp/f/f.ino
+++ temp/f/f.ino
@@ -1,37 +1,97 @@
 #include <Ethernet.h>
 #include <SPI.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHT_1 7
+#define DHT_2 2
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT_Unified dht(DHT_1, DHTTYPE);
+
+uint32_t delayMS;
 
 String apiKeyValue = "tPmAT5Ab3j7F9";
 uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
-const int oneWireBus = 2;     
 
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(oneWireBus);
-// Pass our oneWire reference to Dallas Temperature sensor 
-DallasTemperature sensors(&oneWire);
 EthernetClient client;
 char server[] = "smart.1561.moscow";
 
 void setup() {
   Serial.begin(9600);
-  sensors.begin();
+  dht.begin();
 
   if(Ethernet.begin(mac) == 0){
     Serial.println("Failed to configure Ethernet using DHCP");
     while(1);
   }
+
+   dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {  
-  sensors.requestTemperatures(); 
-  float temperatureC = sensors.getTempCByIndex(0);
-  Serial.print(temperatureC);
-  Serial.println("ºC");
+  float humidity = 0;
+  float temperatureC = 0;
+  //delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    temperatureC = event.temperature;
+    Serial.print(temperatureC);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    humidity = event.relative_humidity;
+    Serial.print(humidity);
+    Serial.println(F("%"));
+  }
+
+
+
   
   String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(temperatureC)
-                           + "&value2=" + String(0) + "&value3=" + String(0) + "";
+                           + "&value2=" + String(humidity) + "&value3=" + String(0) + "";
                            
   if (client.connect(server,80)){
       Serial.println("Connected to server");
@@ -54,5 +114,5 @@ void loop() {
       Serial.print(c);  
     }
  }*/
-  delay(18000);
+  delay(1800000);
 }
