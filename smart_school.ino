b9e2949bc0e4134a49c4aa2546d7cb8cdafd26d0
vanof <58184758+vanof@users.noreply.github.com>
2020-07-17 11:43:52 +0300

    большое обновление

--- smart_school.ino
+++ smart_school.ino
@@ -1,37 +1,40 @@
-#include <Ethernet.h>
+// библиотеки для Ethernet
+// 10 пин используется для CS по умолчанию
+// You can use Ethernet.init(pin) to configure the CS pin
+//Ethernet.init(10);  // Most Arduino shields
 #include <SPI.h>
+#include <Ethernet.h>
+
+// библиотеки для основных датчиков
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
 
-//для калибровки с использованием датчика ds18b20 
+// библиотеки для калибровки с использованием датчика ds18b20 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+//конфигурация датчика ds18b20 
 const int oneWireBus = 2;  
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
 float temperature_calibration = 0.0;
 
-//уникальный ключ для записи в базу данных + разный мак-адрес
-String apiKeyValue = "tPmAT5Ab3j7F9";
-//String apiKeyValue = "tPmAT5Ab3j7C3";
-uint8_t mac[6] = {0x00,0x01,0x02,0x03,0x04,0x05};
-
-EthernetClient client;
-char server[] = "smart.1561.moscow";
-
-#define SENSOR_AMOUNT 3
+// количество датчиков
+#define SENSOR_AMOUNT 4
 #define MAX_SENSOR_AMOUNT 4 
 
+// тип датчика
 #define DHTTYPE    DHT11     // DHT 11
 //#define DHTTYPE    DHT22     // DHT 22 (AM2302)
 //#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
+// подключение
 #define DHTPIN1 7
 #define DHTPIN2 8
 #define DHTPIN3 9
-#define DHTPIN4 10
+//dont use 10 pin! 
+#define DHTPIN4 11
 
 DHT dht[] = {
   {DHTPIN1, DHTTYPE},
@@ -40,18 +43,46 @@ DHT dht[] = {
   {DHTPIN4, DHTTYPE}
 };
 
+// хранение полученных значений
 float humidity[MAX_SENSOR_AMOUNT];
 float temperature[MAX_SENSOR_AMOUNT];
 
+// конфигурация интернет соединения
+// мак адрес для плат должен быть разным!
+// assign a MAC address for the ethernet controller.
+// fill in your address here:
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+//byte mac[] = {0x00,0x01,0x02,0x03,0x04,0x05};
+//byte mac[] = {0x06,0x07,0x02,0x03,0x04,0x05};
+//byte mac[] = {0x02,0x04,0x02,0x03,0x04,0x05};
+
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 0, 177);
+IPAddress myDns(192, 168, 0, 1);
+
+EthernetClient client;
+char server[] = "smart.1561.moscow";
+//IPAddress server(64,131,82,241);
+
+//уникальный ключ для записи в базу данных
+//String apiKeyValue = "tPmAT5Ab3j7F9";  //тестовый ключ
+//String apiKeyValue = "tPmAT5Ab3j7D1";  //первый этаж
+//String apiKeyValue = "tPmAT5Ab3j7D2";  //второй этаж
+//String apiKeyValue = "tPmAT5Ab3j7D3";  //третий этаж
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
 unsigned long lastConnectionTime = 0;           // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10*1000;  // delay between updates, in milliseconds
+const unsigned long postingInterval = 60*1000;  // delay between updates, in milliseconds
 
 void setup() {
+  // start serial port:
   Serial.begin(9600);
-
-  //ds18b20
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
   sensors.begin();
-
   //инициализация датчиков
   for (auto& sensor : dht) {
     sensor.begin();
@@ -65,9 +96,6 @@ void setup() {
   for (int i = 0; i < SENSOR_AMOUNT; i++) {
     temperature[i] = dht[i].readTemperature();
     humidity[i] = dht[i].readHumidity();
-  }
-  
-  for (int i = 0; i < SENSOR_AMOUNT; i++) {
     Serial.print(F("Temperature "));
     Serial.print(i);
     Serial.println(temperature[i]);
@@ -75,37 +103,51 @@ void setup() {
     Serial.print(i);
     Serial.println(humidity[i]);
   }
-  
-  //start the Ethernet connection:
+
+  // start the Ethernet connection:
   Serial.println("Initialize Ethernet with DHCP:");
-  if(Ethernet.begin(mac) == 0){
+  if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
-    while(1);
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip, myDns);
+    Serial.print("My IP address: ");
+    Serial.println(Ethernet.localIP());
+  } else {
+    Serial.print("DHCP assigned IP ");
+    Serial.println(Ethernet.localIP());
   }
-  Serial.print("IP address: ");
-  Serial.println(Ethernet.localIP());  
+  // give the Ethernet shield a second to initialize:
   delay(1000);
 }
 
-void loop() {  
-
-  /*
-  // if ten seconds have passed since your last connection,
-  // then connect again and send data:
-  if (millis() - lastConnectionTime > postingInterval) {
-   httpRequest();  //prototype of void loop
+void loop() {
+  // if there's incoming data from the net connection.
+  // send it out the serial port.  This is for debugging
+  // purposes only:
+  if (client.available()) {
+    char c = client.read();
+    Serial.write(c);
   }
-  */
 
   //опрос ds18b20
   sensors.requestTemperatures(); 
   temperature_calibration = sensors.getTempCByIndex(0);
-
+  
   for (int i = 0; i < SENSOR_AMOUNT; i++) {
     temperature[i] = dht[i].readTemperature();
     humidity[i] = dht[i].readHumidity();
   }
-  
+
   String httpRequestData = "api_key=" + apiKeyValue
                            + "&value1=" + String(temperature[0])
                            + "&value2=" + String(humidity[0])
@@ -118,8 +160,21 @@ void loop() {
                            + "&calibration=" + String(temperature_calibration)
                            + "";
   Serial.println(httpRequestData); 
-  
+
+  httpRequest(httpRequestData);
+
+  delay(30000);
+  //delay(60000);     //1 минута
+  //delay(300000);  //5 минут
+  //delay(900000);  //15 минут частота опроса
+}
+
+// this method makes a HTTP connection to the server:
+void httpRequest(String httpRequestData) {
+  // close any connection before send a new request.
+  // This will free the socket on the WiFi shield
   client.stop();
+
   // if there's a successful connection:
   if (client.connect(server, 80)) {
       Serial.println("Connected to server");
@@ -131,12 +186,18 @@ void loop() {
       client.println();
       client.println(httpRequestData);
       client.println();
-      lastConnectionTime = millis();
+    /*
+    // send the HTTP GET request:
+    client.println("GET /latest.txt HTTP/1.1");
+    client.println("Host: smart.1561.moscow");
+    client.println("User-Agent: arduino-ethernet");
+    client.println("Connection: close");
+    client.println();
+    */
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
   } else {
     // if you couldn't make a connection:
     Serial.println("connection failed");
   }
-  
-  //delay(300000);  //5 минут
-  delay(900000);  //15 минут частота опроса
 }
